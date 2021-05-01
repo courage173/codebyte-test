45042619ca408d39f3fa5a92c30507c971f9fdea
courage173 <courageosemwengie@gmail.com>
2021-05-01 18:52:32 +0100

    added image upload functionality

--- api/user.js
+++ api/user.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const sendErrorResponse = require('../utils/response').sendErrorResponse;
 const sendItemResponse = require('../utils/response').sendItemResponse;
-//const sendListResponse = require("../middlewares/response").sendListResponse;
+const cloudinary = require('cloudinary');
 const MailService = require('../services/mailService');
 const UserService = require('../services/userServices');
 const {
@@ -11,12 +12,23 @@ const {
     validateForgotPasswordCredentials,
     validateResetPasswordCredentials,
 } = require('../middlewares/validateCredentials');
+const { multerUploads } = require('../middlewares/multer');
 const { getUser } = require('../middlewares/user');
+const CLOUD_NAME = process.env['CLOUD_NAME'];
+const API_KEY = process.env['API_KEY'];
+const API_SECRETE = process.env['API_SECRETE'];
+const dataUri = require('../utils/imageCoverter');
 
 // Route
 // Description: registers a user
 // Params:
 // Returns: 400: Error; 500: Server Error; 200: User information.
+
+cloudinary.config({
+    cloud_name: CLOUD_NAME,
+    api_key: API_KEY,
+    api_secret: API_SECRETE,
+});
 router.post('/register', validateRegisterationCredentials, async (req, res) => {
     try {
         const data = req.body;
@@ -59,6 +71,38 @@ router.get('/profile', getUser, async (req, res) => {
     try {
         const userResponse = await UserService.findOneBy({ _id: req.user.id });
         return sendItemResponse(req, res, userResponse);
+    } catch (error) {
+        return sendErrorResponse(req, res, error);
+    }
+});
+
+// Route
+// Description: this uploads image
+// Params:
+// Returns: 400: Error; 500: Server Error; 200: User information.
+router.post('/upload', getUser, multerUploads, async (req, res) => {
+    try {
+        if (req.file) {
+            const file = await dataUri(req);
+            const imageUploadInfo = await cloudinary.v2.uploader.upload(file, {
+                quality: 'auto',
+                transformation: [
+                    { aspect_ratio: '4:3', crop: 'fill' },
+                    { width: 'auto', dpr: 'auto', crop: 'scale' },
+                ],
+            });
+
+            const { secure_url: profilePic } = imageUploadInfo;
+            const userResponse = await UserService.updateOneBy(
+                { _id: req.user.id },
+                { profilePic }
+            );
+            return sendItemResponse(req, res, userResponse);
+        }
+        return sendErrorResponse(req, res, {
+            message: 'image is missing',
+            code: 400,
+        });
     } catch (error) {
         return sendErrorResponse(req, res, error);
     }
